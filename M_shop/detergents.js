a5b53e5e42875f30a18e98f233586a8431f02bf0
MrMayhem <51161382+stathis39@users.noreply.github.com>
2020-07-08 23:34:35 +0300

    Add files via upload

---
+++ M_shop/detergents.js
@@ -0,0 +1,24 @@
+function spitiou(){
+	document.getElementById('spitiou').style.display='block';
+	document.getElementById('plintiriou').style.display='none';
+	document.getElementById('rouxwn').style.display='none';
+	document.getElementById('piatwn').style.display='none';
+}
+function plintiriou(){
+	document.getElementById('spitiou').style.display='none';
+	document.getElementById('plintiriou').style.display='block';
+	document.getElementById('rouxwn').style.display='none';
+	document.getElementById('piatwn').style.display='none';
+}
+function rouxwn(){
+	document.getElementById('spitiou').style.display='none';
+	document.getElementById('plintiriou').style.display='none';
+	document.getElementById('rouxwn').style.display='block';
+	document.getElementById('piatwn').style.display='none';
+}
+function piatwn(){
+	document.getElementById('spitiou').style.display='none';
+	document.getElementById('plintiriou').style.display='none';
+	document.getElementById('rouxwn').style.display='none';
+	document.getElementById('piatwn').style.display='block';
+}
---
+++ M_shop/stationery.js
@@ -0,0 +1,35 @@
+function ugeias(){
+	document.getElementById('ugeias').style.display='block';
+	document.getElementById('kouzinas').style.display='none';
+	document.getElementById('mantilakia').style.display='none';
+	document.getElementById('xartomantila').style.display='none';
+	document.getElementById('xartopetsetes').style.display='none';
+}
+function kouzinas(){
+	document.getElementById('ugeias').style.display='none';
+	document.getElementById('kouzinas').style.display='block';
+	document.getElementById('mantilakia').style.display='none';
+	document.getElementById('xartomantila').style.display='none';
+	document.getElementById('xartopetsetes').style.display='none';
+}
+function mantilakia(){
+	document.getElementById('ugeias').style.display='none';
+	document.getElementById('kouzinas').style.display='none';
+	document.getElementById('mantilakia').style.display='block';
+	document.getElementById('xartomantila').style.display='none';
+	document.getElementById('xartopetsetes').style.display='none';
+}
+function xartomantila(){
+	document.getElementById('ugeias').style.display='none';
+	document.getElementById('kouzinas').style.display='none';
+	document.getElementById('mantilakia').style.display='none';
+	document.getElementById('xartomantila').style.display='block';
+	document.getElementById('xartopetsetes').style.display='none';
+}
+function xartopetsetes(){
+	document.getElementById('ugeias').style.display='none';
+	document.getElementById('kouzinas').style.display='none';
+	document.getElementById('mantilakia').style.display='none';
+	document.getElementById('xartomantila').style.display='none';
+	document.getElementById('xartopetsetes').style.display='block';
+}
