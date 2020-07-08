6feae3676d54884d7906b450738f312a86c09eb1
MrMayhem <51161382+stathis39@users.noreply.github.com>
2020-07-08 23:20:41 +0300

    Add files via upload

---
+++ presents.js
@@ -0,0 +1,35 @@
+function party(){
+	document.getElementById('party').style.display='block';
+	document.getElementById('xristougeniatika').style.display='none';
+	document.getElementById('pasxalina').style.display='none';
+	document.getElementById('apokriatika').style.display='none';
+	document.getElementById('diafora').style.display='none';
+}
+function xristougeniatika(){
+	document.getElementById('party').style.display='none';
+	document.getElementById('xristougeniatika').style.display='block';
+	document.getElementById('pasxalina').style.display='none';
+	document.getElementById('apokriatika').style.display='none';
+	document.getElementById('diafora').style.display='none';
+}
+function pasxalina(){
+	document.getElementById('party').style.display='none';
+	document.getElementById('xristougeniatika').style.display='none';
+	document.getElementById('pasxalina').style.display='block';
+	document.getElementById('apokriatika').style.display='none';
+	document.getElementById('diafora').style.display='none';
+}
+function apokriatika(){
+	document.getElementById('party').style.display='none';
+	document.getElementById('xristougeniatika').style.display='none';
+	document.getElementById('pasxalina').style.display='none';
+	document.getElementById('apokriatika').style.display='block';
+	document.getElementById('diafora').style.display='none';
+}
+function diafora(){
+	document.getElementById('party').style.display='none';
+	document.getElementById('xristougeniatika').style.display='none';
+	document.getElementById('pasxalina').style.display='none';
+	document.getElementById('apokriatika').style.display='none';
+	document.getElementById('diafora').style.display='block';
+}
